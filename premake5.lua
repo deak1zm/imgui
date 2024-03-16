3ace32609077f512a15e7557ebaa12c48a0d73a7
Zachary Deak <deak1zm@gmail.com>
2024-03-16 14:44:32 -0400

    Initial imgui docking branch commit

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+    cppdialect "C++17"
+    staticruntime "On"
+
+    filter "system:macosx"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+
